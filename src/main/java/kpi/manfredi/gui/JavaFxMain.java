370a9082355788bb5c42ea9ee77ef5febd857030
Manfredi <mrmanfredi228@gmail.com>
2020-04-26 15:48:10 +0300

    Added "Home Screen", Context and possibility to switch between scenes

---
+++ src/main/java/kpi/manfredi/gui/JavaFxMain.java
@@ -0,0 +1,22 @@
+package kpi.manfredi.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class JavaFxMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Context.getInstance().setPrimaryStage(primaryStage);
+
+        Context.SceneController sceneController = Context.getInstance().getSceneController();
+        sceneController.addScene(Screen.HOME, FXMLLoader.load(getClass().getResource("/HomeScreen.fxml")));
+        sceneController.addScene(Screen.MAIN, FXMLLoader.load(getClass().getResource("/MainScreen.fxml")));
+        sceneController.activateScene(Screen.HOME);
+
+        primaryStage.setTitle("Knitwear");
+        primaryStage.show();
+    }
+}
