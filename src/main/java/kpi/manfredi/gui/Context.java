018aa1624d8355906e92cf763537efef21edb82b
Manfredi <mrmanfredi228@gmail.com>
2020-04-26 22:55:18 +0300

    Localization added.

--- src/main/java/kpi/manfredi/gui/Context.java
+++ src/main/java/kpi/manfredi/gui/Context.java
@@ -5,28 +5,53 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 
-class Context {
+/**
+ * This class is used to contain configurations to manage application
+ */
+public class Context {
 
     private static int MIN_WIDTH = 400;
-    private static int MIN_HEIGHT = 280;
+    private static int MIN_HEIGHT = 200;
 
     private Stage primaryStage;
     private SceneController sceneController;
+    private List<Locale> availableLocales;
+    private Locale currentLocale;
 
     private static Context instance;
 
-    static Context getInstance() {
+    /**
+     * This method is used to return instance of {@code Context} class
+     *
+     * @return instance of {@code Context} class
+     */
+    public static Context getInstance() {
         if (instance == null) {
             instance = new Context();
         }
         return instance;
     }
 
+    private Context() {
+        availableLocales = new LinkedList<>();
+        availableLocales.add(new Locale("eng"));
+        availableLocales.add(new Locale("ukr"));
+        currentLocale = new Locale("ukr");  // default locale
+    }
+
     void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
 
+    /**
+     * This method is used to return an instance of {@code SceneController}
+     *
+     * @return an instance of {@code SceneController}
+     */
     SceneController getSceneController() {
         if (sceneController == null) {
             sceneController = new SceneController();
@@ -34,6 +59,36 @@ class Context {
         return sceneController;
     }
 
+    /**
+     * This method is used to return available locales
+     *
+     * @return available locales
+     */
+    List<Locale> getAvailableLocales() {
+        return availableLocales;
+    }
+
+    /**
+     * This method is used to return current locale setting
+     *
+     * @return current locale
+     */
+    public Locale getCurrentLocale() {
+        return currentLocale;
+    }
+
+    /**
+     * This method is used to set current locale setting
+     *
+     * @param currentLocale locale to set
+     */
+    public void setCurrentLocale(Locale currentLocale) {
+        this.currentLocale = currentLocale;
+    }
+
+    /**
+     * This class is used to switching between scenes
+     */
     class SceneController {
         private HashMap<Screen, Pane> sceneMap = new HashMap<>();
         private Scene scene;
@@ -41,16 +96,32 @@ class Context {
         private SceneController() {
         }
 
-        void addScene(Screen name, Pane pane) {
-            sceneMap.put(name, pane);
+        /**
+         * This method is used to add scene to map of scenes
+         *
+         * @param key  key of screen
+         * @param pane pane to add
+         */
+        void addScene(Screen key, Pane pane) {
+            sceneMap.put(key, pane);
         }
 
-        protected void removeScene(Screen name) {
-            sceneMap.remove(name);
+        /**
+         * This method is used to remove scene from map of scenes
+         *
+         * @param key key of screen to remove
+         */
+        void removeScene(Screen key) {
+            sceneMap.remove(key);
         }
 
-        void activateScene(Screen name) {
-            scene = new Scene(sceneMap.get(name));
+        /**
+         * This method is used to activate certain screen
+         *
+         * @param key key of screen that will be activated
+         */
+        void activateScene(Screen key) {
+            scene = new Scene(sceneMap.get(key));
             primaryStage.setScene(scene);
             primaryStage.setMinWidth(Math.max(scene.getWidth(), MIN_WIDTH));
             primaryStage.setMinHeight(Math.max(scene.getHeight(), MIN_HEIGHT));
--- src/main/java/kpi/manfredi/gui/HomeScreenController.java
+++ src/main/java/kpi/manfredi/gui/HomeScreenController.java
@@ -3,16 +3,45 @@ package kpi.manfredi.gui;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import kpi.manfredi.utils.MessageUtil;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
+/**
+ * @author manfredi
+ */
 public class HomeScreenController implements Initializable {
     @FXML
     private Button createButton;
 
+    @FXML
+    private Button openButton;
+
+    @FXML
+    private ChoiceBox<Locale> languageChoiceBox;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        refreshLocalization();
+        initLanguageChangeListener();
         createButton.setOnAction(event -> Context.getInstance().getSceneController().activateScene(Screen.MAIN));
     }
+
+    private void refreshLocalization() {
+        createButton.setText(MessageUtil.getMessage("create.new.project"));
+        openButton.setText(MessageUtil.getMessage("open.existing.project"));
+    }
+
+    private void initLanguageChangeListener() {
+        languageChoiceBox.getItems().addAll(Context.getInstance().getAvailableLocales());
+        languageChoiceBox.getSelectionModel().select(Context.getInstance().getCurrentLocale());
+        languageChoiceBox.setOnAction(actionEvent -> {
+            Context.getInstance().setCurrentLocale(languageChoiceBox.getSelectionModel().getSelectedItem());
+            refreshLocalization();
+        });
+    }
+
 }
