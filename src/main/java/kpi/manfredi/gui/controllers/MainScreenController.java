5ca1ce43f2ac27fc365b24f6e1a4cef349493227
Manfredi <mrmanfredi228@gmail.com>
2020-05-16 18:40:56 +0300

    Added draw segment 1

--- src/main/java/kpi/manfredi/gui/controllers/MainScreenController.java
+++ src/main/java/kpi/manfredi/gui/controllers/MainScreenController.java
@@ -2,9 +2,11 @@ package kpi.manfredi.gui.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.text.TextAlignment;
 import kpi.manfredi.gui.Context;
 import kpi.manfredi.model.Comb;
@@ -21,6 +23,9 @@ public class MainScreenController implements Initializable {
     private Data data;
 
     private final int cellSize = 50;
+    private final double bottomPadding = 2.5 * cellSize;
+    private final double rightPadding = 1.5 * cellSize;
+    private final double radius = 20;
     private int widthInCells;
     private int heightInCells;
     private int canvasWidth;
@@ -40,6 +45,7 @@ public class MainScreenController implements Initializable {
         clearCanvas();
         drawGrid();
         drawNumbering();
+        drawCombs();
     }
 
     /**
@@ -100,11 +106,11 @@ public class MainScreenController implements Initializable {
         GraphicsContext context = canvas.getGraphicsContext2D();
         context.setFill(Color.BLACK);
         double radius = 4;
-        double yPoint = canvasHeight - 2.5 * cellSize;
+        double yPoint = canvasHeight - bottomPadding;
         while (yPoint > cellSize) {
-            double xPoint = canvasWidth - 1.5 * cellSize;
+            double xPoint = canvasWidth - rightPadding;
             while (xPoint > 2 * cellSize) {
-                context.fillOval(xPoint - radius, yPoint - radius, radius, radius);
+                context.fillOval(xPoint - radius / 2, yPoint - radius / 2, radius, radius);
                 xPoint -= cellSize;
             }
             yPoint -= cellSize;
@@ -124,7 +130,7 @@ public class MainScreenController implements Initializable {
         context.setTextAlign(TextAlignment.RIGHT);
         int number = 1;
         double xPoint = cellSize * 1.5;
-        double yPoint = canvasHeight - 2.5 * cellSize;
+        double yPoint = canvasHeight - bottomPadding;
         while (yPoint > cellSize) {
             context.strokeText(Integer.toString(number++), xPoint, yPoint);
             yPoint -= cellSize;
@@ -139,12 +145,166 @@ public class MainScreenController implements Initializable {
         context.setTextAlign(TextAlignment.CENTER);
         int number = 0;
         double xPoint = canvasWidth - cellSize;
-        double yPoint = canvasHeight - 1.5 * cellSize;
+        double yPoint = canvasHeight - rightPadding;
         while (xPoint > cellSize) {
             String s = Integer.toString(number++);
 
             context.strokeText(s, xPoint, yPoint);
             xPoint -= cellSize;
         }
+    }
+
+    private void drawCombs() {
+        for (Comb comb : data.getComb()) {
+            drawComb(comb);
+        }
+    }
+
+    private void drawComb(Comb comb) {
+        List<Comb.Row> rows = comb.getRow();
+        for (int i = 0; i < rows.size() - 1; i++) {
+            Comb.Row row = rows.get(i);
+            Comb.Row nextRow = rows.get(i + 1);
+            int shiftComb = (int)(row.getA() - row.getB());
+            int shiftNextComb = (int)(nextRow.getA() - nextRow.getB());
+            int shiftRow = (int)(Math.max(row.getA(), row.getB()) - Math.max(nextRow.getA(), nextRow.getB()));
+            Case theCase = determineTheCase(shiftComb, shiftNextComb, shiftRow);
+
+            drawSegment(theCase, row, i, nextRow);
+        }
+    }
+
+    private Case determineTheCase(int shiftComb, int shiftNextComb, int shiftRow) {
+
+        if (shiftComb > 0 && shiftNextComb > 0 && shiftRow > 0) return Case.CASE1;
+        if (shiftComb < 0 && shiftNextComb < 0 && shiftRow < 0) return Case.CASE2;
+        if (shiftComb > 0 && shiftNextComb > 0 && shiftRow < 0) return Case.CASE3;
+        if (shiftComb < 0 && shiftNextComb < 0 && shiftRow > 0) return Case.CASE4;
+        if (shiftComb > 0 && shiftNextComb < 0 && shiftRow > 0) return Case.CASE5;
+        if (shiftComb < 0 && shiftNextComb > 0 && shiftRow < 0) return Case.CASE6;
+        if (shiftComb > 0 && shiftNextComb < 0 && shiftRow < 0) return Case.CASE7;
+        if (shiftComb < 0 && shiftNextComb > 0 && shiftRow > 0) return Case.CASE8;
+        if (shiftComb == 0 && shiftNextComb > 0 && shiftRow > 0) return Case.CASE9;
+        if (shiftComb == 0 && shiftNextComb < 0 && shiftRow < 0) return Case.CASE10;
+        if (shiftComb == 0 && shiftNextComb > 0 && shiftRow < 0) return Case.CASE11;
+        if (shiftComb == 0 && shiftNextComb < 0 && shiftRow > 0) return Case.CASE12;
+        if (shiftComb > 0 && shiftNextComb == 0 && shiftRow > 0) return Case.CASE13;
+        if (shiftComb < 0 && shiftNextComb == 0 && shiftRow < 0) return Case.CASE14;
+        if (shiftComb > 0 && shiftNextComb == 0 && shiftRow < 0) return Case.CASE15;
+        if (shiftComb < 0 && shiftNextComb == 0 && shiftRow > 0) return Case.CASE16;
+        if (shiftComb == 0 && shiftNextComb == 0 && shiftRow < 0) return Case.CASE17;
+        if (shiftComb == 0 && shiftNextComb == 0 && shiftRow > 0) return Case.CASE18;
+
+        return null;
+    }
+
+    private void drawSegment(Case theCase, Comb.Row row, int rowNum, Comb.Row nextRow) {
+
+        switch (theCase) {
+            case CASE1:
+                drawSegment1(row, rowNum, nextRow);
+                break;
+            case CASE2:
+                break;
+            case CASE3:
+                break;
+            case CASE4:
+                break;
+            case CASE5:
+                break;
+            case CASE6:
+                break;
+            case CASE7:
+                break;
+            case CASE8:
+                break;
+            case CASE9:
+                break;
+            case CASE10:
+                break;
+            case CASE11:
+                break;
+            case CASE12:
+                break;
+            case CASE13:
+                break;
+            case CASE14:
+                break;
+            case CASE15:
+                break;
+            case CASE16:
+                break;
+            case CASE17:
+                break;
+            case CASE18:
+                break;
+        }
+    }
+
+    private void drawSegment1(Comb.Row row, int rowNum, Comb.Row nextRow) {
+        double   endX = getOrigin().getX() - row.getB() * cellSize + radius;
+        double   endY = getOrigin().getY() - rowNum * cellSize;
+
+        double side = getOrigin().getX() - nextRow.getB() * cellSize - endX;
+
+        double angleTo = 
+                Math.PI / 2 +
+                Math.asin(radius / Math.sqrt(cellSize * cellSize + Math.pow(side, 2))) +
+                Math.atan(cellSize / (side));
+
+        List<Point2D> points = MathUtil.getCirclePoints(
+                new Point2D(getOrigin().getX() - nextRow.getB() * cellSize, getOrigin().getY() - (rowNum + 1) * cellSize),
+                radius,
+                0.0,
+                angleTo);
+
+        Point2D lastPoint = new Point2D(endX, endY);
+        
+        points.add(lastPoint);
+        
+        drawContour(points);
+    }
+
+    private void drawContour(List<Point2D> points) {
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        context.setLineWidth(1.0);
+        context.beginPath();
+        for (Point2D point : points) {
+            context.lineTo(point.getX(), point.getY());
+        }
+        context.stroke();
+    }
+
+    private void drawLine(Line line) {
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        context.beginPath();
+        context.moveTo(line.getStartX(), line.getStartY());
+        context.lineTo(line.getEndX(), line.getEndY());
+        context.stroke();
+    }
+    
+    private Point2D getOrigin() {
+        return new Point2D(canvasWidth - rightPadding, canvasHeight - bottomPadding);
+    }
+
+    private enum Case {
+        CASE1,
+        CASE2,
+        CASE3,
+        CASE4,
+        CASE5,
+        CASE6,
+        CASE7,
+        CASE8,
+        CASE9,
+        CASE10,
+        CASE11,
+        CASE12,
+        CASE13,
+        CASE14,
+        CASE15,
+        CASE16,
+        CASE17,
+        CASE18;
     }
 }
--- src/main/java/kpi/manfredi/utils/MathUtil.java
+++ src/main/java/kpi/manfredi/utils/MathUtil.java
@@ -1,5 +1,8 @@
 package kpi.manfredi.utils;
 
+import javafx.geometry.Point2D;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MathUtil {
@@ -55,5 +58,30 @@ public class MathUtil {
             result = getLeastCommonMultiple(result, input.get(i));
         }
         return result;
+    }
+
+    public static List<Point2D> getCirclePoints(Point2D center, double radius, double angleFrom, double angleTo) {
+        List<Point2D> circlePoints = new ArrayList<>();
+        angleFrom = -angleFrom;
+        angleTo = -angleTo;
+        double currentAngle = angleFrom;
+        if (angleFrom < angleTo) {
+            while (currentAngle <= angleTo + 0.001) {
+                double xLocation = radius * Math.cos(currentAngle) + center.getX();
+                double yLocation = radius * Math.sin(currentAngle) + center.getY();
+                Point2D temp = new Point2D(xLocation, yLocation);
+                circlePoints.add(temp);
+                currentAngle += Math.PI / 80;
+            }
+        } else {
+            while (currentAngle >= angleTo - 0.001) {
+                double xLocation = radius * Math.cos(currentAngle) + center.getX();
+                double yLocation = radius * Math.sin(currentAngle) + center.getY();
+                Point2D temp = new Point2D(xLocation, yLocation);
+                circlePoints.add(temp);
+                currentAngle -= Math.PI / 80;
+            }
+        }
+        return circlePoints;
     }
 }
